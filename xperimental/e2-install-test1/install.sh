37b0337f5f58a2af7ea084f02d3e8a9d5ea38693
Andrei DAMIAN <andrei@lummetry.ai>
2024-01-22 22:23:44 +0200

    fix install url

--- xperimental/e2-install-test1/install.sh
+++ xperimental/e2-install-test1/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L "https://raw.githubusercontent.com/Lummetry/aixp_devops/main/aixp_factory/install-factory.sh?$RANDOM" -o install-factory.sh
+curl -L "https://raw.githubusercontent.com/Lummetry/aixp_devops/main/aixp_factory/other/install-factory.sh?$RANDOM" -o install-factory.sh
 chmod +x install-factory.sh
 ./install-factory.sh
