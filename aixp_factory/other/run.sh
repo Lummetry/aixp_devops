3727e150df8064925de83c0990e927cd61c1e6c8
Andrei DAMIAN <andrei@lummetry.ai>
2024-01-22 23:01:35 +0200

    fix run

--- aixp_factory/other/run.sh
+++ aixp_factory/other/run.sh
@@ -1,2 +1,6 @@
 mkdir -p logs
-ANSIBLE_FORCE_COLOR=true ansible-playbook deploy.yml | tee "./logs/ansible_$(date +%Y_%m_%d__%H_%M).log"
+# check if logs folder contains ansible.log and rename it to ansible_YYYY_MM_DD__HH_MM.log
+if [ -f "./logs/ansible.log" ]; then
+    mv "./logs/ansible.log" "./logs/ansible_$(date +%Y_%m_%d__%H_%M).log"
+fi
+ANSIBLE_FORCE_COLOR=true ansible-playbook deploy.yml | tee "./logs/ansible.log"
